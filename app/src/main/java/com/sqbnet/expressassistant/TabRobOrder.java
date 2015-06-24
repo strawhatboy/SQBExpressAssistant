b5c08d8754dd1ef56fb6af8370b8da36ac8709d4
Andy Cui <strawhat2010@live.cn>
2015-06-24 22:54:29 +0800

    added radar effect

--- app/src/main/java/com/sqbnet/expressassistant/TabRobOrder.java
+++ app/src/main/java/com/sqbnet/expressassistant/TabRobOrder.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.sqbnet.expressassistant.controls.CircleImageView;
@@ -22,6 +26,9 @@ public class TabRobOrder extends Fragment {
     private TextView tv_waiting;
     private TextView tv_hint;
     private TextView tv_hint_content;
+
+    private ImageView iv_radar;
+    private Animation animation;
 
     public TabRobOrder() {
         // Required empty public constructor
@@ -41,6 +48,12 @@ public class TabRobOrder extends Fragment {
         tv_waiting = (TextView) view.findViewById(R.id.tv_searching_waiting);
         tv_hint = (TextView) view.findViewById(R.id.tv_searching_hint);
         tv_hint_content = (TextView) view.findViewById(R.id.tv_searching_hint_content);
+        iv_radar = (ImageView) view.findViewById(R.id.iv_searching_radar);
+
+
+        animation = AnimationUtils.loadAnimation(this.getActivity(), R.animator.razor_rolling);
+        LinearInterpolator lin = new LinearInterpolator();
+        animation.setInterpolator(lin);
     }
 
     public boolean getIsWaiting() {
@@ -54,11 +67,16 @@ public class TabRobOrder extends Fragment {
             tv_waiting.setVisibility(View.VISIBLE);
             tv_hint.setVisibility(View.INVISIBLE);
             tv_hint_content.setVisibility(View.INVISIBLE);
+            iv_radar.setVisibility(View.VISIBLE);
+            iv_radar.clearAnimation();
+            iv_radar.setAnimation(animation);
         } else {
             civ_avatar.setVisibility(View.INVISIBLE);
             tv_waiting.setVisibility(View.INVISIBLE);
             tv_hint.setVisibility(View.VISIBLE);
             tv_hint_content.setVisibility(View.VISIBLE);
+            iv_radar.setVisibility(View.INVISIBLE);
+            iv_radar.clearAnimation();
         }
     }
 }
